4c63b0f4fe955da93e862cccfaf7859b14e295ff
xjubep <xjubep@gmail.com>
2020-07-05 18:35:21 +0900

    [boj 3053] 택시 기하학

---
+++ boj_3053.cpp
@@ -0,0 +1,13 @@
+// 택시 기하학 (herman)
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+int main(void) {
+    int r;
+
+    scanf("%d", &r);
+    printf("%f\n%f\n", (double)r*r*M_PI, (double)2*r*r);
+
+    return 0;
+}
