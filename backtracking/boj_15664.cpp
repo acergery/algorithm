3617054b15ffccd2e53cb37fa6fc41ca3ea2addb
xjubep <xjubep@gmail.com>
2020-07-15 19:28:57 +0900

    [boj 15664] N과 M (10)

---
+++ backtracking/boj_15664.cpp
@@ -0,0 +1,42 @@
+// N과 M (10)
+// N과 M (9)에서 비내림차순 조건 추가됨
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX_N = 8, MX_M = 8;
+int n, m;
+int arr[MX_M], input[MX_N];
+bool isUsed[MX_N];
+
+void func(int k, int start) {
+    if (k == m) {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    bool check[10000+1] = {false, };
+
+    for (int i = start; i < n; i++) {
+        if (!isUsed[i] && !check[input[i]]) {
+            arr[k] = input[i];
+            isUsed[i] = true;
+            check[input[i]] = true;
+            func(k+1, i+1);
+            isUsed[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) 
+        cin >> input[i];
+    sort(input, input+n);
+    func(0, 0);
+
+    return 0;
+}
